7a36c6034d229d17b5d35f47b6321c2d064dad02
shoaib-ytel <shoabi@ytel.co.in>
2015-12-08 07:47:37 +0530

    Remove All Authentication

--- Message360Development/src/main/java/com/M360/api/configuration/M360Constants.java
+++ Message360Development/src/main/java/com/M360/api/configuration/M360Constants.java
@@ -6,15 +6,11 @@ import java.util.Locale;
 public class M360Constants {
 	//development
 	public static final String BASE_URL = "https://api-dev.message360.com";
-	public static final String API_VERSION = "api/v1b";
+	public static final String API_VERSION = "api/v1b";	
 	
-	
-	//public static final String ACCOUNTSID1="YTaa972478db58e151d9c67a3521e6d7191";
-	//public static final String ACCOUNTSID="YTaa972478db58e151d9c67a3521e6d719";//rizwan@xoyal.c at development
-	//public static final String AUTHTOKEN="f03ad426b8775a7ef81b53593727476e";//
-		
-	public static final String ACCOUNTSID="YT94c49d220e5a45dc516f9733460460f5";//sangeeth@xoyal.c at development
-	public static final String AUTHTOKEN="53ee61684ef2a3805fb4721dfdf9672f";//
+	// use credential
+	public static final String ACCOUNTSID="{ACCOUNTSID}";//
+	public static final String AUTHTOKEN="{AUTHTOKEN}";//
 		
 	public static final boolean JSONFORMAT=true;
 	
